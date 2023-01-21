aa7cf8a31c1208595c28709401b4cbe5028bebba
Olaxemax <jibaro4u2004@gmail.com>
2023-01-21 02:37:17 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
         roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                            
                             'C': 100, 'D': 500, 'M': 1000}
         result = 0
         item = 0
         roman = roman_string
         if type(roman) is not str or len(roman) is 0:
                 return 0
-        for item in range(item, len(roman)):
+for item in range(item, len(roman)):
                 if item < len(roman) - 1\
-                   and\
+        and\
                    roman_dictionary[roman[item]] <\
-                   roman_dictionary[roman[item + 1]]:
+        roman_dictionary[roman[item + 1]]:
                    result -= roman_dictionary[roman[item]]
-                else:
+else:
                         result += roman_dictionary[roman[item]]
                 return result
